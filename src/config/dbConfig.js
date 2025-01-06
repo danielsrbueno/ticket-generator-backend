1b182a087a01f0adcadacd09fdb9b52a64a589f4
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-01-06 19:43:46 -0300

    fix: change dotenv

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+const user = process.env.DB_USER
+const pass = process.env.DB_PASS
+const dbName = process.env.DB_NAME
+
 const config = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_NAME}.mt53o.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`
 
 const con = mongoose.connect(config, {
