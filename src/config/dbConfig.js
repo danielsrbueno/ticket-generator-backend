95bc5da93e1343e795dc61726378d8451a78fac1
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-01-06 20:18:23 -0300

    chore: change port

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-const user = process.env.DB_USER
-const pass = process.env.DB_PASS
-const dbName = process.env.DB_NAME
-
-const config = `mongodb+srv://${user}:${pass}@${dbName}.mt53o.mongodb.net/?retryWrites=true&w=majority&appName=${dbName}`
+const config = "mongodb+srv://danielsrbueno:z4VPi03I7u2UULhw@ticket.mt53o.mongodb.net/?retryWrites=true&w=majority&appName=ticket"
 
 const con = mongoose.connect(config, {
     useNewUrlParser: true,
